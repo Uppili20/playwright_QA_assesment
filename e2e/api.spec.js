2f83c6abceaac7ffcce1c7f44e4d06086fe2c8ea
Uppili <saravananuppili@gmail.com>
2026-04-06 00:17:52 +0530

    updated

--- e2e/api.spec.js
+++ e2e/api.spec.js
@@ -2,11 +2,11 @@ const { test, expect } = require('@playwright/test');
 
 test('API Flow - Create, Get, Update User', async ({ request }) => {
 
-  // 🔹 Create User
+  
   const createRes = await request.post('https://reqres.in/api/users', {
     headers: {
       'Content-Type': 'application/json',
-      'x-api-key': 'reqres_5831f7e7e6e645559c81066b7f78de90'   // required for latest reqres
+      'x-api-key': 'reqres_5831f7e7e6e645559c81066b7f78de90'   
     },
     data: {
       name: 'Uppili',
@@ -21,30 +21,27 @@ test('API Flow - Create, Get, Update User', async ({ request }) => {
 
   console.log('Created User ID:', userId);
 
-  // Validate response fields
+
   expect(createBody.name).toBe('Uppili');
   expect(createBody.job).toBe('QA Engineer');
 
-  // 🔹 Get User (mock API → may not return created user)
+  
   const getRes = await request.get(`https://reqres.in/api/users/${userId}`,{
     headers: {
       'Content-Type': 'application/json',
-      'x-api-key': 'reqres_5831f7e7e6e645559c81066b7f78de90'   // required for latest reqres
+      'x-api-key': 'reqres_5831f7e7e6e645559c81066b7f78de90'   
     }
   });
   
-
-  // Reqres limitation → allow both
   expect([200, 404]).toContain(getRes.status());
 
   const getBody = await getRes.json();
   console.log('Get Response:', getBody);
 
-  // 🔹 Update User
   const updateRes = await request.put(`https://reqres.in/api/users/${userId}`, {
     headers: {
       'Content-Type': 'application/json',
-      'x-api-key': 'reqres_5831f7e7e6e645559c81066b7f78de90'   // required for latest reqres
+      'x-api-key': 'reqres_5831f7e7e6e645559c81066b7f78de90'   
     },
     data: {
       name: 'Saravanan Updated',
@@ -56,9 +53,11 @@ test('API Flow - Create, Get, Update User', async ({ request }) => {
 
   const updateBody = await updateRes.json();
 
-  // Validate updated data
+  
   expect(updateBody.name).toBe('Saravanan Updated');
   expect(updateBody.job).toBe('Senior QA');
 
+
   console.log('Updated Response:', updateBody);
+  console.log("")
 });
